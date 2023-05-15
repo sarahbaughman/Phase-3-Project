047dcaa7b104219b79ab38c588475a34e2721dc3
waylon wickline <waylonw2077@LAPTOP-6MQVH55Q>
2023-05-15 13:21:18 -0400

    added CLI functionality

---
+++ lib/travel_cli.py
@@ -0,0 +1,77 @@
+import argparse
+from country import Country
+from city import City
+from place import Place
+
+def create_country(args):
+    country_manager = Country()
+    country_manager.add_country(args.name)
+    country_manager.close_connection()
+
+def list_countries(args):
+    country_manager = Country()
+    country_manager.list_countries()
+    country_manager.close_connection()
+
+def create_city(args):
+    city_manager = City()
+    city_manager.add_city(args.name, args.country_id)
+    city_manager.close_connection()
+
+def list_cities(args):
+    city_manager = City()
+    city_manager.list_cities(args.country_id)
+    city_manager.close_connection()
+
+def create_place(args):
+    place_manager = Place()
+    place_manager.add_place(args.name, args.city_id)
+    place_manager.close_connection()
+
+def list_places(args):
+    place_manager = Place()
+    place_manager.list_places(args.city_id)
+    place_manager.close_connection()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Travel Database CLI')
+
+    subparsers = parser.add_subparsers(title='Commands', dest='command')
+
+    # Create Country command
+    create_country_parser = subparsers.add_parser('create-country', help='Create a new country')
+    create_country_parser.add_argument('name', type=str, help='Country name')
+    create_country_parser.set_defaults(func=create_country)
+
+    # List Countries command
+    list_countries_parser = subparsers.add_parser('list-countries', help='List all countries')
+    list_countries_parser.set_defaults(func=list_countries)
+
+    # Create City command
+    create_city_parser = subparsers.add_parser('create-city', help='Create a new city')
+    create_city_parser.add_argument('name', type=str, help='City name')
+    create_city_parser.add_argument('country_id', type=int, help='ID of the country')
+    create_city_parser.set_defaults(func=create_city)
+
+    # List Cities command
+    list_cities_parser = subparsers.add_parser('list-cities', help='List all cities')
+    list_cities_parser.add_argument('country_id', type=int, nargs='?', help='ID of the country')
+    list_cities_parser.set_defaults(func=list_cities)
+
+    # Create Place command
+    create_place_parser = subparsers.add_parser('create-place', help='Create a new place')
+    create_place_parser.add_argument('name', type=str, help='Place name')
+    create_place_parser.add_argument('city_id', type=int, help='ID of the city')
+    create_place_parser.set_defaults(func=create_place)
+
+    # List Places command
+    list_places_parser = subparsers.add_parser('list-places', help='List all places')
+    list_places_parser.add_argument('city_id', type=int, nargs='?', help='ID of the city')
+    list_places_parser.set_defaults(func=list_places)
+
+    args = parser.parse_args()
+
+    if args.command:
+        args.func(args)
+    else:
+        parser.print_help()
